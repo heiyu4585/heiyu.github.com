6346d8da6916c64cd9d8ef2c4c4ced13ee5c8c72
黑雨 <wangning4567@163.com>
2016-04-01 00:33:52 +0800

    完成16

--- baiduife/js-16.js
+++ baiduife/js-16.js
@@ -13,14 +13,53 @@ var aqiData = {};
  * 然后渲染aqi-list列表，增加新增的数据
  */
 function addAqiData() {
+  var city = trimStr(document.getElementById("aqi-city-input").value);
+  var value= trimStr(document.getElementById("aqi-value-input").value);
+  var re=/^[A-Za-z]+$/;
+  for(var i= 0,j=0;i<city.length;i++){
+    str = city.substr(i,1);
+    console.log(str);
+    if( !/^[\u4e00-\u9fa5]+$/i.test(str) && !re.test(str)){
+      alert("只能是中英文");
+      return;
+    }
+  }
 
+  if(!isPositiveNum(value)){
+     alert("不是整数");
+    return;
+  }
+  //去掉前后空格
+  function trimStr(str){return str.replace(/(^\s*)|(\s*$)/g,"");}
+  aqiData[city] = value;
+
+  console.log(aqiData);
+}
+//是否为正整数
+function isPositiveNum(s){
+  var re = /^[0-9]*[1-9][0-9]*$/ ;
+  return re.test(s)
 }
 
 /**
  * 渲染aqi-table表格
  */
 function renderAqiList() {
+  var table =  document.getElementById("aqi-table");
+  var oTr =document.createElement("tr");
+  table.innerHTML=" <tr><td>城市</td><td>空气质量</td><td>操作</td></tr>"
+  table.appendChild(oTr);
+  for( x in aqiData){
+    var oTr =document.createElement("tr");
+    oTr.innerHTML=" <tr><td>"+x+"</td><td>"+aqiData[x]+"</td><td><button dd="+x+">删除</button></td></td></tr>"
+    table.appendChild(oTr);
+  }
 
+  var oDelBtn=document.getElementById("aqi-table").getElementsByTagName('button');
+  var len = oDelBtn.length;
+  for(var i=0;i<len;i++){
+    oDelBtn[i].addEventListener("click",delBtnHandle);
+  }
 }
 
 /**
@@ -38,17 +77,30 @@ function addBtnHandle() {
  */
 function delBtnHandle() {
   // do sth.
+  this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
+ var x = this.getAttribute("dd");
+   delete aqiData[x];
 
   renderAqiList();
 }
 
 function init() {
+  window.onload=function(){
 
-  // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
-  document.getElementById("#add-btn").addEventListener("onclick",addBtnHandle);
-  // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
+console.log(aqiData);
+    // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
+    document.getElementById("add-btn").addEventListener("click",addBtnHandle);
 
+    // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
+   var oDelBtn=document.getElementById("aqi-table").getElementsByTagName('button');
+    var len = oDelBtn.length;
+    if(len){
+      for(var i=0;i<len;i++){
+        oDelBtn[i].addEventListener("click",delBtnHandle());
+      }
 
+    };
+  }
 }
 
 init();
