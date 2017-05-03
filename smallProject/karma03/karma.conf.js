5e211f57dc8e0dd24814755849b17fff9ff37aeb
wangning <wning@allinmd.cn>
2017-05-03 21:26:35 +0800

    生成报告第二版 无头浏览器测试中

---
+++ smallProject/karma03/karma.conf.js
@@ -0,0 +1,61 @@
+// Karma configuration
+// Generated on Wed May 03 2017 17:16:34 GMT+0800 (中国标准时间)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+      frameworks: ['mocha', 'chai'],
+
+    // list of files / patterns to load in the browser
+      files: [
+          'karma-bundle.js'
+      ],
+
+    // list of files to exclude
+    exclude: [
+    ],
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+      preprocessors: { 'karma-bundle.js': ['webpack','coverage'] },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress','coverage'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+    htmlReporter: {
+        outputDir: 'report/ut',
+        reportName: 'result' //outputDir+reportName组成完整的输出报告格式，如没有定义，会自动生成浏览器+OS信息的文件夹，不方便读取报告
+    },
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+      coverageReporter:{
+          type:'html',
+          dir:'coverage/',
+      },
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+};
---
+++ smallProject/karma03/src/unit/add.spec.js
@@ -0,0 +1,8 @@
+var add = require('../common.js');
+
+describe('add unit test.', function(){
+    it('2 + 3 = 5', function(){
+        var result = add(2, 3);
+        expect( result).to.be.equal(14 );
+    });
+});
