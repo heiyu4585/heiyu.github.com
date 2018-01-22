9c35b917d13a232150c9ad34c16ad5a8df1309ba
wangning <wangning1@allinmd.cn>
2018-01-22 10:15:29 +0800

    vue_express 前后端架构

---
+++ smallProject/vue_express/src/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <div>hello , this app vue3333!</div>
+        <router-view></router-view>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "app"
+    }
+</script>
+
+<style scoped>
+    body{color:red;}
+</style>
