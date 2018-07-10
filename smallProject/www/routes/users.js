ff98b198d73f392e4963b4197a7fe6e979ee41eb
wangning <wangning1@allinmd.cn>
2018-07-10 18:47:59 +0800

    进度更新

--- smallProject/www/routes/users.js
+++ smallProject/www/routes/users.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+    res.json({
+        a:1,
+        b:2
+    })
 });
-
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+    // res.send('respond with a resource');
+    res.json({
+        a:"post",
+        b:2
+    })
+});
 module.exports = router;
